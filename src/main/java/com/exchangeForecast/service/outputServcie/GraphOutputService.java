8274aff1907aa6739419b59c2487be82d5fedf7a
robert <robby300im@gmail.com>
2022-03-11 23:05:08 +0500

    add logic to GraphOutputService

--- src/main/java/com/exchangeForecast/service/outputServcie/GraphOutputService.java
+++ src/main/java/com/exchangeForecast/service/outputServcie/GraphOutputService.java
@@ -7,6 +7,7 @@ import com.github.sh0nk.matplotlib4j.PythonExecutionException;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,24 +16,23 @@ public class GraphOutputService implements OutputService {
     @Override
     public void output(Update update, SendBotMessageService sendBotMessageService, List<Rate> rates) {
         List<Double> x = new ArrayList<>();
-        for (int i = 1; i < 8; i++) {
-            x.add(((double) i) / 10);
+        for (int i = 1; i < rates.size() + 1; i++) {
+            x.add((double) i);
         }
-        List<Double> C = rates.stream().map(rate -> rate.getExchangeRate().doubleValue()).collect(Collectors.toList());
+
+        List<Double> y = rates.stream().map(rate -> rate.getExchangeRate().doubleValue()).collect(Collectors.toList());
 
 
         Plot plt = Plot.create();
-        plt.plot().add(x, C);
 
-
-        plt.title("histogram");
+        plt.plot().add(x, y);
+        plt.xlabel("Days:");
+        plt.ylabel("Rate:");
+        plt.title("Exchange forecast for " + rates.get(0).getCurrency());
         plt.savefig("/tmp/histogram.png").dpi(200);
-// Don't miss this line to output the file!
         try {
             plt.executeSilently();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (PythonExecutionException e) {
+        } catch (IOException | PythonExecutionException e) {
             e.printStackTrace();
         }
         sendBotMessageService.sendPhoto(update.getMessage().getChatId().toString(), "histogram.png");
