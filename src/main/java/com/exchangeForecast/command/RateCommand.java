d2dcceec7f23067f153cda9694ad714036d988d1
robert <robby300im@gmail.com>
2022-03-14 15:21:08 +0500

    fix help command

--- src/main/java/com/exchangeForecast/command/RateCommand.java
+++ src/main/java/com/exchangeForecast/command/RateCommand.java
@@ -4,12 +4,8 @@ import com.exchangeForecast.cash.RatesCash;
 import com.exchangeForecast.domain.Currency;
 import com.exchangeForecast.domain.ForecastPeriod;
 import com.exchangeForecast.domain.Rate;
-import com.exchangeForecast.service.forecastService.ActualForecastService;
+import com.exchangeForecast.parser.RateCommandPartsParser;
 import com.exchangeForecast.service.forecastService.ForecastService;
-import com.exchangeForecast.service.forecastService.LinearRegressionForecastService;
-import com.exchangeForecast.service.forecastService.MoonForecastService;
-import com.exchangeForecast.service.outputServcie.GraphOutputService;
-import com.exchangeForecast.service.outputServcie.ListOutputService;
 import com.exchangeForecast.service.outputServcie.OutputService;
 import com.exchangeForecast.service.outputServcie.SendBotMessageService;
 import com.github.sh0nk.matplotlib4j.PythonExecutionException;
@@ -21,10 +17,9 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
+import java.time.Period;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Setter
 @Getter
@@ -38,6 +33,7 @@ public class RateCommand implements Command {
     private final SendBotMessageService sendBotMessageService;
     private final RatesCash cash;
 
+
     public RateCommand(SendBotMessageService sendBotMessageService, RatesCash cash) {
         this.cash = cash;
         this.sendBotMessageService = sendBotMessageService;
@@ -45,51 +41,16 @@ public class RateCommand implements Command {
 
     @Override
     public void execute(Update update) {
-        String[] messageArgs = update.getMessage().getText().split("\\s");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String cdxArgument = messageArgs[1];
-        String timeLine = messageArgs[2];
-        String timeLineArgument = messageArgs[3];
-        String alg = messageArgs[4];
-        String algArgument = messageArgs[5];
-        String output = messageArgs[6];
-        String outputArgument = messageArgs[7];
-        String[] cdxLines = cdxArgument.split(",", 5);
-        setCdx(Arrays.stream(cdxLines).map(Currency::ofConsoleName).collect(Collectors.toList()));
-        switch (timeLine) {
-            case "-period":
-                setPeriod(ForecastPeriod.ofName(timeLineArgument));
-                setDate(null);
-                break;
-            case "-date":
-                if (timeLineArgument.equals("tomorrow")) {
-                    setDate(LocalDate.now().plusDays(1));
-                } else setDate(LocalDate.parse(timeLineArgument, formatter));
-                setPeriod(null);
-        }
-        if (alg.equals("-alg")) {
-            switch (algArgument) {
-                case "linear":
-                    setAlgorithm(new LinearRegressionForecastService());
-                    break;
-                case "actual":
-                    setAlgorithm(new ActualForecastService());
-                    break;
-                case "moon":
-                    setAlgorithm(new MoonForecastService());
-            }
-        }
-        if (output.equals("-output")) {
-            switch (outputArgument) {
-                case "list":
-                    setOutputMethod(new ListOutputService());
-                    break;
-                case "graph":
-                    setOutputMethod(new GraphOutputService());
-            }
-        }
+        RateCommandPartsParser rateCommandPartsParser = new RateCommandPartsParser();
+        HashMap<String, Object> rateCommandParts = rateCommandPartsParser.getRateCommandParts(update);
+        algorithm = (ForecastService) rateCommandParts.get("algorithm");
+        cdx = (List<Currency>) rateCommandParts.get("cdx");
+        period = (ForecastPeriod) rateCommandParts.get("period");
+        date = (LocalDate) rateCommandParts.get("date");
+        outputMethod = (OutputService) rateCommandParts.get("outputMethod");
+
         List<List<Rate>> listsOfRates = algorithm.forecast(cash, cdx, period, date);
-        logger.info("Данные спрогнозированны алгоритмом:  {}." + algorithm.getClass().getSimpleName());
+        logger.info("Данные спрогнозированны алгоритмом:  {}.", algorithm.getClass().getSimpleName());
         try {
             outputMethod.output(update, sendBotMessageService, listsOfRates);
             logger.info("Данные направлены на вывод сервисом: {}.", outputMethod.getClass().getSimpleName());
@@ -97,4 +58,6 @@ public class RateCommand implements Command {
             e.printStackTrace();
         }
     }
+
+
 }
---
+++ src/main/java/com/exchangeForecast/parser/RateCommandPartsParser.java
@@ -0,0 +1,86 @@
+package com.exchangeForecast.parser;
+
+import com.exchangeForecast.domain.Currency;
+import com.exchangeForecast.domain.ForecastPeriod;
+import com.exchangeForecast.exceptions.NotValidException;
+import com.exchangeForecast.service.forecastService.ActualForecastService;
+import com.exchangeForecast.service.forecastService.ForecastService;
+import com.exchangeForecast.service.forecastService.LinearRegressionForecastService;
+import com.exchangeForecast.service.forecastService.MoonForecastService;
+import com.exchangeForecast.service.outputServcie.GraphOutputService;
+import com.exchangeForecast.service.outputServcie.ListOutputService;
+import com.exchangeForecast.service.outputServcie.OutputService;
+import lombok.Setter;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Setter
+public class RateCommandPartsParser {
+    private List<Currency> cdx;
+    private ForecastPeriod period;
+    private LocalDate date;
+    private ForecastService algorithm;
+    private OutputService outputMethod;
+
+    public HashMap<String, Object> getRateCommandParts(Update update) {
+        HashMap<String, Object> partsOfCommandInMap = new HashMap<>();
+        String[] messageArgs = update.getMessage().getText().split("\\s");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String cdxArgument = messageArgs[1];
+        String timeLine = messageArgs[2];
+        String timeLineArgument = messageArgs[3];
+        String alg = messageArgs[4];
+        String algArgument = messageArgs[5];
+        String output = messageArgs[6];
+        String outputArgument = messageArgs[7];
+        String[] cdxLines = cdxArgument.split(",", 5);
+        setCdx(Arrays.stream(cdxLines).map(Currency::ofConsoleName).collect(Collectors.toList()));
+        switch (timeLine) {
+            case "-period":
+                setPeriod(ForecastPeriod.ofName(timeLineArgument));
+                setDate(null);
+                break;
+            case "-date":
+                if (timeLineArgument.equals("tomorrow")) {
+                    setDate(LocalDate.now().plusDays(1));
+                } else setDate(LocalDate.parse(timeLineArgument, formatter));
+                setPeriod(null);
+        }
+        if (alg.equals("-alg")) {
+            switch (algArgument) {
+                case "linear":
+                    setAlgorithm(new LinearRegressionForecastService());
+                    break;
+                case "actual":
+                    setAlgorithm(new ActualForecastService());
+                    break;
+                case "moon":
+                    setAlgorithm(new MoonForecastService());
+            }
+        }
+        if (output.equals("-output")) {
+            switch (outputArgument) {
+                case "list":
+                    setOutputMethod(new ListOutputService());
+                    break;
+                case "graph":
+                    if (date == null) {
+                        setOutputMethod(new GraphOutputService());
+                    } else throw new NotValidException("График строится только на периоде.");
+            }
+        }
+        partsOfCommandInMap.put("algorithm", algorithm);
+        partsOfCommandInMap.put("cdx", cdx);
+        partsOfCommandInMap.put("period", period);
+        partsOfCommandInMap.put("date", date);
+        partsOfCommandInMap.put("outputMethod", outputMethod);
+        return partsOfCommandInMap;
+    }
+
+}
